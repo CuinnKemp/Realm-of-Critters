9f59ed9a05b10b74781c8c268dcbc96e4a88cbc4
David M <david.maslov@student.adelaide.edu.au>
2022-10-18 22:56:14 +1030

    fixed compile message at the start

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,5 @@
 // g++ Game.cpp ResourceManager.cpp Player.cpp UIManager.cpp Enemy.cpp
-// Enemies.cpp Beast.cpp Slime.cpp Obstacle.cpp ObstacleGenerator.cpp Arrow.cpp
+// Enemies.cpp Beast.cpp Slime.cpp GoldenSlime.cpp Obstacle.cpp ObstacleGenerator.cpp Arrow.cpp
 // SpinningBlade.cpp SpinningBladeSpawner.cpp ExpBall.cpp ExpContainer.cpp
 // ExpSpawner.cpp PlayerArrow.cpp PlayerArrowSpawner.cpp -lsfml-graphics
 // -lsfml-window -lsfml-system -lsfml-audio
